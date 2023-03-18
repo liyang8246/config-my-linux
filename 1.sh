0414f29ab2ab5e4b7054c9caae3a37fcd0a88cfa
LiYang <LiYang@DESKTOP-O4VAPJ7.localdomain>
2023-03-19 00:16:38 +0800

    2

--- 1.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/liyang8246/config-my-linux.git
-git push -u origin master
---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+
+def get_sources(name,version,source):
+    sources = []
+    sources.append(f'deb {source}{name}/ {version} main restricted universe multiverse')
+    sources.append(f'deb {source}{name}/ {version}-updates main restricted universe multiverse')
+    sources.append(f'deb {source}{name}/ {version}-backports main restricted universe multiverse')
+    sources.append(f'deb {source}{name}/ {version}-security main restricted universe multiverse')
+    return sources
+
+if __name__ == '__main__':
+    info = {
+        'name':'',
+        'version':'',
+    }
+    
+    names = ['debian','ubuntu','arch*','manjaro']
+    [print(f'{i}) {name}') for i,name in enumerate(names)]
+    print('Please select your system')
+    info['name'] = names[int(input(f'input 0-{len(names)-1}: '))]
+    with open('test.list','r') as f:
+        info['version'] = [i[i.index('=')+1:-1] for i in f.readlines() if 'VERSION_CODENAME=' in i][0]
+    
+    if input('change sources\n(y/n): ') == 'y':
+        if info['name'] in ['ubuntu','debian']:
+            os.system('cp /etc/apt/sources.list /etc/apt/sources.list.bak')
+            sources_list = [
+                ('THU ','https://mirrors.tuna.tsinghua.edu.cn/'),
+                ('USTC','https://mirrors.ustc.edu.cn/')
+            ]
+            [print(f'{i}) {sources_name[0]}') for i,sources_name in enumerate(sources_list)]
+            sources = get_sources(info['name'],info['version'],sources_list[int(input(f'input 0-{len(names)-1}: '))][1])
+            with open('/etc/apt/sources.list','w') as f:
+                [f.writelines(f'{i}\n') for i in sources]
+            os.system('apt update && apt install git wget -y')
